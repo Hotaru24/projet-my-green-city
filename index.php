b20d11b218d136bd917bef317cf9080ffba644de
Jennifer Btk <j.betkaoui.wcs@gmail.com>
2019-09-17 14:47:39 +0200

    Correction indentation affichage

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
-  
-  	<head>
+
+	<head>
 		<meta charset="utf-8">
 		<title>My Green City</title>
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
@@ -10,22 +10,22 @@
 		<meta name="description" content="My Green City blablabla">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    	<link rel="stylesheet" href="common.css">
+		<link rel="stylesheet" href="common.css">
 		<link rel="stylesheet" href="style.css">
-		
+
 		<!--Google Fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,900" rel="stylesheet">
-    	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet"> 
 	</head>
 
 	<body>
-	    <section id="ctn_header"></section>
-	      <div id="container">
-	        <section id="ctn_action"></section>
-	        <section id="ctn_association"></section>
-	        <section id="ctn_dechet"></section>
-	        <section id="ctn_contact"></section>
-	      </div><!-- Fin Container -->
-	    <section id="ctn_footer"></section>
+		<section id="ctn_header"></section>
+		<div id="container">
+			<section id="ctn_action"></section>
+			<section id="ctn_association"></section>
+			<section id="ctn_dechet"></section>
+			<section id="ctn_contact"></section>
+		</div><!-- Fin Container -->
+		<section id="ctn_footer"></section>
 	</body>
 </html>
