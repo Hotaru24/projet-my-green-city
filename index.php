53b45f6ae41d2cd21c3f38f37e61419e6f6020e1
Jennifer Btk <j.betkaoui.wcs@gmail.com>
2019-09-17 11:52:21 +0200

    First commit index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  	<head>
+		<meta charset="utf-8">
+		<title>My Green City</title>
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+		<meta name="author" content="#">
+		<meta name="description" content="My Green City blablabla">
+
+    <link rel="stylesheet" href="common.css">
+		<link rel="stylesheet" href="style.css">
+		
+		<!--Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet"> 
+
+	</head>
+
+	<body>
+    <section id="ctn_header"></section>
+      <div id="container">
+        <section id="ctn_action"></section>
+        <section id="ctn_association"></section>
+        <section id="ctn_dechet"></section>
+        <section id="ctn_contact"></section>
+      </div><!-- Fin Container -->
+    <section id="ctn_footer"></section>
