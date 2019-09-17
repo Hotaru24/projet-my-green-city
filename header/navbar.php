220afa253c201b92f104c8f80ba8f25ba7d1d2b9
Madeline Joubert <madelinejoubert44@gmail.com>
2019-09-17 17:14:43 +0200

    Create folder header and files for navbar and banner. Navbar ok on desktop, just design on mobile.

---
+++ header/navbar.php
@@ -0,0 +1,18 @@
+<head>
+    <link rel="stylesheet" href="styleNavbar.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<h1>My green city</h1>
+<nav>
+    <div></div>
+    <div></div>
+    <div></div>
+    <ul>
+        <li><a class="siteName" href="#home">My green city</a></li>
+        <li><a href="#actions">Actions</a></li>
+        <li><a href="#associations">Associations</a></li>
+        <li><a href="#dechets">Déchets</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+</nav>
