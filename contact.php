8213641df23986ceb9f4e4f01ac8d342f1650c92
Jennifer Btk <j.betkaoui.wcs@gmail.com>
2019-09-18 15:31:15 +0200

    Ajout bloc contact/footer et style.css

--- contact.php
+++ contact.php
@@ -1,25 +1,11 @@
-<!DOCTYPE html>
-<html lang="fr">
-
 	<head>
-		<meta charset="utf-8">
-		<title>My Green City</title>
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-
-		<meta name="author" content="#">
-		<meta name="description" content="My Green City blablabla">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<link rel="stylesheet" href="common.css">
-		<link rel="stylesheet" href="reset.css">
-
 		<link rel="stylesheet" href="style_jennifer.css">
 
 		<!--Google Fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,900" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet"> 
 	</head>
-
+	
 	<body>
 		<section id="ctn_header"></section>
 		<div id="container">
