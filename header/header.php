b3ef3f717787941ab2548534ea49997906a1f955
Madeline Joubert <madelinejoubert44@gmail.com>
2019-09-18 12:23:01 +0200

    Finish design navbar. Removes files banner and add thisfiles in the files navbar. Rename files navbar in header.

--- header/header.php
+++ header/header.php
@@ -3,16 +3,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<h1>My green city</h1>
 <nav>
-    <div></div>
-    <div></div>
-    <div></div>
-    <ul>
-        <li><a class="siteName" href="#home">My green city</a></li>
+    <a href="" class="logo">My green city</a>
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
         <li><a href="#actions">Actions</a></li>
         <li><a href="#associations">Associations</a></li>
         <li><a href="#dechets">Déchets</a></li>
         <li><a href="#contact">Contact</a></li>
     </ul>
 </nav>
+
+<div class="banner">
+    <img src="https://via.placeholder.com/1000x250" />
+</div>
